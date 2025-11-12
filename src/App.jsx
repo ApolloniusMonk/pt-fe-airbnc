5cb36aa722330b7c34c07104c3a4378ad04e073a
ADMIN <admin@iMac.broadband>
2025-11-12 12:51:17 +0000

    added some placeholder component files and pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router";
+
+import Header from "./components/Header";
+import FilterBar from "./components/FilterBar";
 import "./App.css";
 
 function App() {
-  return <h1>Hello there</h1>;
+  return (
+    <div className="App">
+      <header className="Header">
+        <Header />
+      </header>
+      <section className="FilterBar">
+        <FilterBar />
+      </section>
+    </div>
+  );
 }
 
 export default App;
